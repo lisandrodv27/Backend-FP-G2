3df7f0eb91f9c25f600a479381f455be56109d3f
shaquille-tech <62995981+shaquille-tech@users.noreply.github.com>
2020-11-30 16:23:50 +0000

    Amendments

--- src/main/java/com/qa/HP/rest/TraineesController.java
+++ src/main/java/com/qa/HP/rest/TraineesController.java
@@ -1,8 +1,12 @@
 package com.qa.HP.rest;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.qa.HP.domain.Trainees;
 import com.qa.HP.service.TraineesService;
@@ -18,12 +22,12 @@ public class TraineesController {
 
 	//Create a trainee- signing up
 	@PostMapping("/create")
-	public ResponseEntity<Trainees> createTrainee(@RequestBody Trainee trainee) {
-		return new ResponseEntity<Trainee>(this.service.createTrainee(trainee), HttpStatus.CREATED);
+	public ResponseEntity<Trainees> createTrainee(@RequestBody Trainees trainee) {
+		return new ResponseEntity<Trainees>(this.service.createTrainee(trainee), HttpStatus.CREATED);
 	}
 	//Get trainee name when signing in
 	@GetMapping("/get")
-	public ResponseEntity<List<Trainee>> getTrainee() {
+	public ResponseEntity<List<Trainees>> getTrainee() {
 		return ResponseEntity.ok(this.service.getTrainee());
 	}
 
