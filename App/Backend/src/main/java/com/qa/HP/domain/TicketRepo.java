e57af434dd3c200a1a0e7bc163ba022f9c87d248
lisandrodv27 <lvillarruel@qa.com>
2020-12-08 15:31:38 +0000

    taking care of git

--- App/Backend/src/main/java/com/qa/HP/domain/TicketRepo.java
+++
@@ -1,8 +0,0 @@
-package com.qa.HP.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//JPA allows java to communicate to DB
-public interface TicketRepo extends JpaRepository<Ticket, Long> {
-
-}
